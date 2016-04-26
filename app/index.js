2a5d5fb3397b80df0a09a150c28345b8157dbb1a
Neil Atkinson <neil@bluespot.io>
2016-04-26 13:13:39 +0100

    Add a logger as thunk middleware

--- app/index.js
+++ app/index.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import createLogger from 'redux-logger'
+
 import ScheduleApp from './containers/ScheduleApp'
 
+const logger = createLogger()
+
 const store = createStore(
-  () => ({})
+  () => ({}), // reducer that does nothing
+  {}, // initial state
+  applyMiddleware(
+    thunk,
+    logger
+  )
 )
 
 ReactDom.render(
