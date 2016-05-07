50d1eabadb6eeba5566029da8b419db14c5f481f
Neil Atkinson <neil@bluespot.io>
2016-05-07 12:04:28 +0100

    Whoops. Correct activity ids in initialState

--- app/initialState.js
+++ app/initialState.js
@@ -16,10 +16,10 @@ const streams = {
 const activities = {
   ids: [1,2,3,4],
   lookup: {
-    1: {id: 2, name: 'Project 1'},
-    2: {id: 3, name: 'Project 2'},
-    3: {id: 4, name: 'Project 3'},
-    4: {id: 6, name: 'Admin'},
+    1: {id: 1, name: 'Project 1'},
+    2: {id: 2, name: 'Project 2'},
+    3: {id: 3, name: 'Project 3'},
+    4: {id: 4, name: 'Admin'},
   }
 }
 
