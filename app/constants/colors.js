fb3d3f0f2de2386b4bc1954ba49ee2e312f557ed
Neil Atkinson <neil@bluespot.io>
2016-05-08 20:57:10 +0100

    Use primary color rather than specify the color.
    This works better when the style changes.

--- app/constants/colors.js
+++ app/constants/colors.js
@@ -1,7 +1,5 @@
 import {
-  cyan400,
   darkBlack,
-  pink200,
 } from 'material-ui/styles/colors'
 
 import { fade } from 'material-ui/utils/colorManipulator'
@@ -9,8 +7,8 @@ import { fade } from 'material-ui/utils/colorManipulator'
 import { palette } from '../themes/customLightTheme'
 
 export default Object.assign({
-  scheduledColor: fade(cyan400, 0.4),
-  barColor: fade(cyan400, 0.9),
+  scheduledColor: fade(palette.primary1Color, 0.4),
+  barColor: fade(palette.primary1Color, 0.9),
   streamDisabledColor: fade(darkBlack, 0.2),
   streamColor: fade(darkBlack, 0.06),
 }, palette)
