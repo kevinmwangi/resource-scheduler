e4eac581602e63bce1cfa787d5b8a6a18cea76f7
Neil Atkinson <neil@bluespot.io>
2016-05-13 15:54:59 +0100

    Reset the form after updating selected stream days.

--- app/reducers/form/index.js
+++ app/reducers/form/index.js
@@ -1,3 +1,6 @@
-import {reducer as formReducer} from 'redux-form';
+import {reducer as formReducer} from 'redux-form'
+import streamDaysSettings from './streamDaysSettings'
 
-export default formReducer
+export default formReducer.plugin({
+  streamDaysSettings,
+})
