31a20b7038516aca45de6ae7ca8bd047aca40801
Neil Atkinson <neil@bluespot.io>
2016-05-15 18:19:43 +0100

    Re-skin

--- app/components/ScheduleAppBar.js
+++ app/components/ScheduleAppBar.js
@@ -4,6 +4,8 @@ import IconMenu from 'material-ui/IconMenu'
 import MenuItem from 'material-ui/MenuItem'
 import IconButton from 'material-ui/IconButton'
 import NavigationMenu from 'material-ui/svg-icons/navigation/menu'
+import Update from 'material-ui/svg-icons/action/update'
+import FontIcon from 'material-ui/FontIcon'
 
 import colors from '../constants/colors'
 
@@ -35,14 +37,19 @@ export default function ScheduleAppBar (props) {
       zDepth={2}
       title="Scheduler"
       iconElementLeft={
+        <IconButton>
+          <Update color={colors.alternateTextColor} />
+        </IconButton>
+      }
+      iconElementRight={
         <IconMenu
           iconButtonElement={
             <IconButton>
               <NavigationMenu color={colors.alternateTextColor} />
             </IconButton>
           }
-          anchorOrigin={{horizontal: 'left', vertical: 'top'}}
-          targetOrigin={{horizontal: 'left', vertical: 'top'}}
+          anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+          targetOrigin={{horizontal: 'right', vertical: 'top'}}
           style={styles.IconMenu}
           iconStyle={styles.iconStyle}
           onItemTouchTap={handleMenuSelection}
@@ -50,7 +57,6 @@ export default function ScheduleAppBar (props) {
           <MenuItem key={'resource'} primaryText='New Resource'/>
           <MenuItem key={'activity'} primaryText='New Activity' />
         </IconMenu>
-
       }
       style={Object.assign({}, styles.AppBar, layoutStyles)}
     />
--- app/themes/customLightTheme.js
+++ app/themes/customLightTheme.js
@@ -1,17 +1,17 @@
 import {
-  cyan500, cyan700,
+  lightBlue500, lightBlue700,
   grey100, grey300, grey400, grey500,
-  pinkA200,
+  deepOrangeA200,
   white, darkBlack, fullBlack,
 } from 'material-ui/styles/colors';
 
 import { fade } from 'material-ui/utils/colorManipulator'
 
 export const palette = {
-  primary1Color: cyan500,
-  primary2Color: cyan700,
+  primary1Color: lightBlue500,
+  primary2Color: lightBlue700,
   primary3Color: grey400,
-  accent1Color: pinkA200,
+  accent1Color: deepOrangeA200,
   accent2Color: grey100,
   accent3Color: grey500,
   textColor: darkBlack,
@@ -19,7 +19,7 @@ export const palette = {
   canvasColor: white,
   borderColor: grey300,
   disabledColor: fade(darkBlack, 0.3),
-  pickerHeaderColor: cyan500,
+  pickerHeaderColor: lightBlue500,
   clockCircleColor: fade(darkBlack, 0.07),
   shadowColor: fullBlack,
 }
