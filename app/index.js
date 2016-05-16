1f5e1f1c6b0dcdcd55778112cefb928a009db3e2
Neil Atkinson <neil@bluespot.io>
2016-05-16 11:16:30 +0100

    Use the setDateRange action creator to initialise data.

--- app/index.js
+++ app/index.js
@@ -6,11 +6,13 @@ import thunk from 'redux-thunk'
 import createLogger from 'redux-logger'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import moment from 'moment'
 
 import initialState from './initialState'
 import reducers from './reducers'
 import ScheduleApp from './containers/ScheduleApp'
 import customLightTheme from './themes/customLightTheme';
+import setDateRange from './actionCreators/commands/setDateRange'
 
 const logger = createLogger()
 
@@ -22,6 +24,12 @@ const store = createStore(
     logger
   )
 )
+
+const periodLength = 120
+const halfPeriod = moment.duration(periodLength / 2, 'days')
+const startDate = moment().startOf('day').subtract(halfPeriod).toISOString()
+const endDate = moment().startOf('day').add(halfPeriod).toISOString()
+store.dispatch(setDateRange({startDate, endDate}))
 
 // Required by some material-react components
 var injectTapEventPlugin = require("react-tap-event-plugin");
--- app/initialState.js
+++ app/initialState.js
@@ -21,76 +21,7 @@ const resources = {
   }
 }
 
-const periodLength = 120
-const today = moment().startOf('day')
-const halfPeriod = moment.duration(periodLength / 2, 'days')
-const periodStart = today.subtract(halfPeriod)
-
-function generateDates(periodStart, periodLength) {
-  let ids = []
-
-  for(let i = 0; i < periodLength; i++) {
-    ids.push(periodStart.add(1, 'day').format('YYYY-MM-DD'))
-  }
-  return ids
-}
-
-const dates = generateDates(periodStart, periodLength)
-
-function generateSingleResourceActivityDay (resource_id, activity_id, idCounter, date, dateToBeWorked, dateToBeScheduled) {
-  const isPast = moment(date).isBefore(moment(), 'day')
-  const hours = (isPast && dateToBeWorked) ? Math.round(Math.random() * 6) : undefined
-
-  const uid = `${date}:${resource_id}:${activity_id}`
-
-  return {
-    id: idCounter,
-    resource_id,
-    activity_id,
-    date,
-    hours,
-    uid,
-    scheduled: dateToBeScheduled,
-  }
-}
-
-const resourceActivityDays = (function generateResourceActivityDays () {
-  let lookup = {}
-  let idCounter = 0
-
-  resources.ids.forEach((resource_id) => {
-    activities.ids.forEach((activity_id) => {
-      const streamToBeScheduled = Math.random() > 0.5
-
-      if (streamToBeScheduled) {
-        dates.forEach((date) => {
-          const dayOfWeek = moment(date).day()
-          const isWeekday = dayOfWeek > 0 && dayOfWeek < 6
-          const dateToBeScheduledOrWorked = Math.random() > 0.2 && isWeekday
-
-          if (dateToBeScheduledOrWorked) {
-            idCounter++
-            const dateToBeWorked = Math.random() > 0.1
-            const dateToBeScheduled = Math.random() > 0.1 && isWeekday
-            const scheduledDay = generateSingleResourceActivityDay(
-              resource_id, activity_id, idCounter, date, dateToBeWorked, dateToBeScheduled
-            )
-
-            lookup[scheduledDay.uid] = scheduledDay
-          }
-        })
-      }
-    })
-  })
-
-  return {
-    lookup,
-  }
-})()
-
 export default {
   resources,
   activities,
-  dates,
-  resourceActivityDays,
 }
